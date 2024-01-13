06ad7dd9cba6fbea03c06bd03d81d14f63db4155
Lyj <345462993@qq.com>
2024-01-13 11:12:43 +0800

    1

--- Application/board.h
+++ Application/board.h
@@ -1,10 +1,5 @@
 #include "stm32f0xx_ll_gpio.h"
 
-/**************************
- * 按键 端口配置
- *************************/
-#define LIGHT_BTN_PORT (GPIOB)
-#define LIGHT_BTN_PIN (LL_GPIO_PIN_0)
 
 /**************************
  * led 端口配置
